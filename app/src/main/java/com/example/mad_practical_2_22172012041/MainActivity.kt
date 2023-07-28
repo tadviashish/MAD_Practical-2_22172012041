67e1553019ac5d0ce9396024c039f58fb2dd641d
student <student@gnu.ac.in>
2023-07-28 12:39:03 +0530

    commit done

---
+++ app/src/main/java/com/example/mad_practical_2_22172012041/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mad_practical_2_22172012041
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate Method is called")
+    }
+
+    fun showMessage(message:String)
+    {
+        Log.i(TAG, message)
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showMessage("onStart Method is called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
